13a01e6e376baad00ae79552cce3ea217c1d2dc3
akashgreninja <akashuhulekal@gmail.com>
2023-04-16 00:04:06 +0530

    fixed all comments

--- src/ivr/handler.js
+++ src/ivr/handler.js
@@ -33,47 +33,6 @@ exports.menu = function menu(digit) {
   return optionActions[digit] ? optionActions[digit]() : redirectWelcome();
 };
 
-// exports.planets = function planets(digit) {
-//   const optionActions = {
-//     2: "+19295566487",
-//     3: "+17262043675",
-//     4: "+16513582243",
-//   };
-
-//   if (optionActions[digit]) {
-//     const twiml = new VoiceResponse();
-//     twiml.dial(optionActions[digit]);
-//     return twiml.toString();
-//   }
-
-//   return redirectWelcome();
-// };
-
-// /**
-//  * Returns Twiml
-//  * @return {String}
-//  */
-// function giveExtractionPointInstructions() {
-//   const twiml = new VoiceResponse();
-
-//   twiml.say(
-//     "To get to your extraction point, get on your bike and go down " +
-//       "the street. Then Left down an alley. Avoid the police cars. Turn left " +
-//       "into an unfinished housing development. Fly over the roadblock. Go " +
-//       "passed the moon. Soon after you will see your mother ship.",
-//     { voice: "alice", language: "en-GB" }
-//   );
-
-//   twiml.say(
-//     "Thank you for calling the ET Phone Home Service - the " +
-//       "adventurous alien's first choice in intergalactic travel"
-//   );
-
-//   twiml.hangup();
-
-//   return twiml.toString();
-// }
-
 
 /**
  * Returns a TwiML to interact with the client
